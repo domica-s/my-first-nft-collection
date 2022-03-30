37212cfaaaef610328f1f6c70d9ef8a1588fa1ab
Domica <1155130940@link.cuhk.edu.hk>
2022-03-30 15:44:45 +0800

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc072446aC95fd3B8fEb2a633cCB2A54ffa1Eac89";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
